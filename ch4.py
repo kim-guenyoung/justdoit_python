c18dff435934fa3392b02b3dce520c5a4b290c1d
kim-guenyoung <101184338+kim-guenyoung@users.noreply.github.com>
2022-04-11 07:21:29 +0900

    Add files via upload

---
+++ ch4.py
@@ -0,0 +1,46 @@
+#    print("Hello")
+#    print("World!")
+#else :
+#    print("Goodbye")
+#    print("World!")
+
+#item_one = 1
+#item_two = 2
+#item_three = 3
+#otal = item_one + \
+#    item_two + \
+#        item_three
+#print(total)
+
+days = ['Mon', 'Tue', 'Wed', 
+'Thu', 'Fri', 'Sat', 
+'Sun']
+print(days)
+
+print("hello"); print("World!")
+
+z1 = '''This is
+a book.'''
+type(z1)
+print(z1)
+print(type(z1))
+
+x = None
+if (x == None):
+    print('x == None!')
+
+if (x is None):
+    print('x is None!')
+
+print(ord("A"))
+print(hex(65))
+print(chr(65))
+print(chr(0x41))
+print(ord('\n'))
+print(u'\u000a')
+
+print('\ud55C\uae00')
+
+
+import keyword
+print(keyword.kwlist)
